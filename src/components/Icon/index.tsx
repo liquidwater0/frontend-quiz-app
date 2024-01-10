16f2ad0010b96ccedb55e6b7be00a5e3a8b4edf8
Brandon <32186774+liquidwater0@users.noreply.github.com>
2024-01-09 21:26:12 -0600

    make icon a span instead of a div

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,18 +1,18 @@
 import "./index.scss";
 import type { ReactNode, HTMLAttributes } from "react";
 
-interface IconProps extends HTMLAttributes<HTMLDivElement> {
+interface IconProps extends HTMLAttributes<HTMLSpanElement> {
     iconStyle?: string,
     icon: ReactNode
 }
 
 export default function Icon({ iconStyle, icon, className, ...props }: IconProps) {
     return (
-        <div 
+        <span 
             className={`icon ${className ? className : ""} ${iconStyle ? iconStyle : ""}`}
             { ...props }
         >
             { icon }
-        </div>
+        </span>
     );
 }
